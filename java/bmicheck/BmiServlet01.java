1a0e80ddbd9b6b9620637da1ead0e635f1e54832
ikeshin-asase <99540305+ikeshin-asase@users.noreply.github.com>
2023-02-20 21:02:41 +0900

    Update BmiServlet01.java
    数値の四捨五入するように訂正

--- java/bmicheck/BmiServlet01.java
+++ java/bmicheck/BmiServlet01.java
@@ -29,12 +29,18 @@ public class BmiServlet01 extends HttpServlet {
      */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         request.setCharacterEncoding("UTF-8");
-        double inputHeight = Double.parseDouble(request.getParameter("height")); // 入力値の代入
-        double inputWeight = Double.parseDouble(request.getParameter("weight"));
-        double bmiSum = inputWeight / Math.pow((inputHeight /100),2); // bmiの計算
-        request.setAttribute("height",inputHeight); // 値の引き渡し
-        request.setAttribute("weight",inputWeight);
-        request.setAttribute("bmi",bmiSum);
+        // 四捨五入して各値を産出
+        double height = ((double)Math.round(Double.parseDouble(request.getParameter("height")) * 100))/100;
+        double weight = ((double)Math.round(Double.parseDouble(request.getParameter("weight")) * 100))/100;
+        double bmi = ((double)Math.round(weight / Math.pow((height /100),2) * 100))/100;
+        double appropriateWeight = ((double)Math.round((Math.pow(height /100,2)*22) * 100))/100;
+        double comparison = ((double)Math.round((appropriateWeight - weight) * 100))/100;
+
+        request.setAttribute("height",height); // 値の引き渡し
+        request.setAttribute("weight",weight);
+        request.setAttribute("bmi",bmi);
+        request.setAttribute("appropriateWeight",appropriateWeight);
+        request.setAttribute("comparison",comparison);
 
         RequestDispatcher dispatcher = request.getRequestDispatcher("/WEB-INF/bmicheck/bmi_result.jsp");
         dispatcher.forward(request,response);
